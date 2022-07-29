3e87f509bc983d319811a626d47589a27ab4efa5
br00klyn93 <bmclaury93@gmail.com>
2022-07-29 17:26:50 +0200

    Update indexrate.py

--- indexrate.py
+++ indexrate.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    console.log("All working!")
+    print("All working!")
     return render_template('index.html')
 
 if __name__ == "__main__":
